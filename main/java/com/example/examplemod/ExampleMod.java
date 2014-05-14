ed500e22a1dddeb3628d1b0688b242d54ccaeb7d
Doug Hamilton <doug.hamil@gmail.com>
2014-05-13 20:11:17 -0600

    Message on login

--- main/java/com/example/examplemod/ExampleMod.java
+++ main/java/com/example/examplemod/ExampleMod.java
@@ -1,9 +1,14 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.util.ChatComponentText;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -12,9 +17,25 @@ public class ExampleMod
     public static final String VERSION = "1.0";
     
     @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	FMLCommonHandler.instance().bus().register(new ExampleEventListener());
+    }
+    
+    @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
-}
+    
+    public static class ExampleEventListener
+    {
+    	@SubscribeEvent
+    	public void onPLayerLogin(PlayerEvent.PlayerLoggedInEvent event)
+    	{
+    		event.player.addChatComponentMessage(new ChatComponentText("If you're seeing this, that means Modcraft is loaded!"));
+    	}
+    	
+    }
+ }
